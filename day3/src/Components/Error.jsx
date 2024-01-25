a5848e60a9298dc1d9579287073dacf8d6f23e02
drishtikundra <dkundra_be20@thapar.edu>
2024-01-26 01:11:06 +0530

    error included

--- day3/src/Components/Error.jsx
+++ day3/src/Components/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Error() {
   return (
     <div className="alert alert-danger" role="alert">
-      No TodoList
+      Nothing to display ! Check your to-do list.
     </div>
   );
 }
